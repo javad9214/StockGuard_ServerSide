38768b6b44aa5b7f9aaf06853a3a9733a7be923e
Mohammad Javad <javadhosseini14@gmail.com>
2025-09-15 16:05:58 +0330

    chore: initial Spring Boot project

---
+++ src/main/java/com/stockguard/StockGuardApplication.java
@@ -0,0 +1,13 @@
+package com.stockguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockGuardApplication.class, args);
+	}
+
+}
